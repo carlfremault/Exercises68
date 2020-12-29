4f4a7f23dc03860b03a18bb3984a1d5e9f488687
Carl Fremault <carlfremault@yahoo.com>
2020-12-29 15:59:39 +0100

    Ajoutez des fichiers projet.

---
+++ Exercises68/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a few numbers separated by a hyphen :");
+            var input = Console.ReadLine();
+            var numbers = new List<int>();
+
+            foreach(var number in input.Split('-'))
+            {
+                numbers.Add(Convert.ToInt32(number));
+            }
+
+            numbers.Sort();
+
+            var consecutive = true;
+
+            for (var i = 1; i < numbers.Count(); i++)
+            {
+                if (numbers[i] - 1 != numbers[i - 1])
+                {
+                    consecutive = false;
+                    break;
+                }
+            }
+
+            var message = consecutive ? "Consecutive" : "Not Consecutive";
+            Console.WriteLine(message);
+
+            
+       
+        }
+    }
+}
